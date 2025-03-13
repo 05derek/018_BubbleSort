e1f0fbd00f170788da93c9b4ea86e5f2615d3b04
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-03-13 11:10:40 +0700

    nProsedur input

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -1,6 +1,22 @@
-#include <iostream>
-#include <string>
-using namspace std;
-                       
-int(20);  // Deklarasi variabel global array a dengan ukuran 20
-int n;      // Deklarasi variabel global n untuk menyimpan banyknya elemen pada array
+
+
+void input(){
+    while(true){
+        cout << "Masukkan banyknya elemen pada array: "<< endl;
+        cin >> n;
+        if (n=<20)
+        break;
+        else{
+            cout << "\nArray dapat mempunyai maksimal 20 elemen. \n";
+        }
+    }
+    cout << endl;
+    cout << "======================" << endl;
+    cout << "Masukkan elemen array " << endl;
+    cout << "======================" << endl;
+
+    for (int 1 = 0; 1 <n; 1++){
+        cout << "data ke" << (1+1) << ";";
+        cin >> arr {i};
+    }
+}
