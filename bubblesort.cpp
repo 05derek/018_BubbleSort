dec423eed3de9d119257f54519b38344674a5aa6
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-03-13 11:22:34 +0700

    step 1

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -1,22 +1,8 @@
 
-
-void input(){
-    while(true){
-        cout << "Masukkan banyknya elemen pada array: "<< endl;
-        cin >> n;
-        if (n=<20)
-        break;
-        else{
-            cout << "\nArray dapat mempunyai maksimal 20 elemen. \n";
-        }
-    }
-    cout << endl;
-    cout << "======================" << endl;
-    cout << "Masukkan elemen array " << endl;
-    cout << "======================" << endl;
-
-    for (int 1 = 0; 1 <n; 1++){
-        cout << "data ke" << (1+1) << ";";
-        cin >> arr {i};
-    }
 }
+
+void bubbleSortArray()
+{
+    int pass = 1; //step 1
+
+    
