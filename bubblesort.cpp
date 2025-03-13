a183e8cd552e70004cffbc8f3582651482e3dcf4
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-03-13 11:51:44 +0700

    menampilkan display

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -1,5 +1,32 @@
 
-} while (pass <= n - 1); // step 5
+void display(){
+    cout <<endl;
+    cout << "=================" << endl;
+    cout << "elemen Array yang telah tersusun" << endl;
+    cout << "=================" << endl;
+    cout << endl;
+    for (int j = 0; j < n; j++){
+        cout << aee[j]; //menampilkan array
+        cout << "-->";
+    }
+
+cout << endl;
+cout << endl;
+
+cout << "jumlah pass =" << n - 1 << endl; // menampilkan jumlah dari pass
+cout << endl;
+cout << endl;
+}
+
+int main (){
+    input();
+
+    bubbleSortArray();
+    display();
+
+    system("pause");
+    return 0;
+}
 
             
     
