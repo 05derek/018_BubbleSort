05aab2717b7765d0cc00fe7711656efaf662033c
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-03-13 11:06:49 +0700

    Deklarasi Variable Global

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -2,3 +2,5 @@
 #include <string>
 using namspace std;
                        
+int(20);  // Deklarasi variabel global array a dengan ukuran 20
+int n;      // Deklarasi variabel global n untuk menyimpan banyknya elemen pada array
